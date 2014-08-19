6ca623fe804e44ff8bb80e972e0f76a73d680a6e
Nick DiRienzo <nicholas.dirienzo@optimizely.com>
2014-08-18 19:48:06 -0700

    mailchimp

--- f2014/js/splash.js
+++ f2014/js/splash.js
@@ -1,3 +1,10 @@
+function afterSignup() {
+  $('footer').fadeOut(1000);
+  $('.header').fadeOut(1000, function(){
+    $('.afterSignup').fadeIn(1000);
+  });
+}
+
 $(document).ready(function(){
 	$('.moreInfo').hide();
 	$('.afterSignup').hide();
@@ -10,14 +17,14 @@ $(document).ready(function(){
 
 	});
 
-	$(document).keypress(function(e){
-		if(e.which == 13){
-			$('footer').fadeOut(1000);
-			$('.header').fadeOut(1000, function(){
-				$('.afterSignup').fadeIn(1000);
-			});
-
-		}
-	});
+  $("#mc-embedded-subscribe-form").ajaxChimp({
+    url: "http://ubhacking.us8.list-manage.com/subscribe/post?u=8a8ff97ccff01e95b486c9589&amp;id=db15220939",
+    callback: function(resp) {
+      if (resp.msg.indexOf("is already subscribed") > -1 || resp.msg.indexOf("Too many subscribe attempts") > -1) {
+        $(".afterSignup").text("You are already subscribed to our mailing list. We'll reach out once we have more to say!");
+      }
+      afterSignup();
+    }
+  });
 
 });
